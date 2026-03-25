4e1f1bcc1d43b3b808914e778e47c51cbecd9b95
Paulo Fernando Maggio <paullomagio@gmail.com>
2026-03-25 17:01:58 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,10 @@
 module java_jdbc {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens application to javafx.graphics, javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+    opens gui to javafx.fxml;
+    exports gui;
+
+    opens application to javafx.graphics, javafx.fxml;
 }
