7c2f3bec8c322ffc50cbd1f43c59b4034f09113c
Paulo Fernando Maggio <paullomagio@gmail.com>
2026-04-01 16:30:24 -0300

    SellerForm

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,16 +1,14 @@
 package model.dao;
 
 import java.util.List;
-
 import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
-
 	void insert(Seller obj);
 	void update(Seller obj);
-	void deleteById(Integer id);
-	Seller findById(Integer id);
+	void deleteById(Long id);
+	Seller findById(Long id);
 	List<Seller> findAll();
 	List<Seller> findByDepartment(Department department);
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -12,6 +12,7 @@ import java.util.Map;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -43,7 +44,7 @@ public class SellerDaoJDBC implements SellerDao {
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
-					obj.setId(rs.getInt(1));
+					obj.setId(rs.getLong(1));
 				}
 				DB.closeResultSet(rs);
 			} else {
@@ -68,7 +69,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setLong(5, obj.getDepartment().getId());
-			st.setInt(6, obj.getId());
+			st.setLong(6, obj.getId());
 			
 			st.executeUpdate();
 		} catch (SQLException e) {
@@ -79,21 +80,21 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 	@Override
-	public void deleteById(Integer id) {
+	public void deleteById(Long id) {
 		PreparedStatement st = null;
 		try {
 			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
-			st.setInt(1, id);
+			st.setLong(1, id);
 			st.executeUpdate();
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 		}
 	}
 
 	@Override
-	public Seller findById(Integer id) {
+	public Seller findById(Long id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
@@ -101,7 +102,7 @@ public class SellerDaoJDBC implements SellerDao {
 					"SELECT seller.*, department.Name as DepName FROM seller " +
 					"INNER JOIN department ON seller.DepartmentId = department.Id " +
 					"WHERE seller.Id = ?");
-			st.setInt(1, id);
+			st.setLong(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
 				Department dep = instantiateDepartment(rs);
@@ -118,7 +119,7 @@ public class SellerDaoJDBC implements SellerDao {
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller obj = new Seller();
-		obj.setId(rs.getInt("Id"));
+		obj.setId(rs.getLong("Id"));
 		obj.setName(rs.getString("Name"));
 		obj.setEmail(rs.getString("Email"));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
